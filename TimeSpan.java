973158e04a9d68ef35f53b15ac4da20f01ad141a
Michael Ramirez <ramirezmike2@gmail.com>
2012-10-24 08:41:25 -0400

    Barebones TimeSpan complete

--- TimeSpan.java
+++ TimeSpan.java
@@ -1,4 +1,36 @@
 public class TimeSpan
 {
+    private double timeIn;
+    private double timeOut;
+    
+    public TimeSpan(double tIn, double tOut)
+    {
+        setTime(tIn, tOut);
+    }
 
+    public void setTime(double tIn, double tOut)
+    {
+        if (checkTimes(tIn, tOut))
+        {
+            timeIn = tIn;
+            timeOut = tOut;
+        }
+        else
+            System.out.println("Time in < Time Out");// implement error throw
+    }
+
+    public double getTimeIn()
+    {
+        return timeIn;
+    }
+
+    public double getTimeOut()
+    {
+        return timeOut;
+    }
+
+    private boolean checkTimes(double tIn, double tOut)
+    {
+       return (tIn < tOut); 
+    }
 }
